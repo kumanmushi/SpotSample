75e471aa532383502008bb703f9f3110c18d8ec9
村田真矢 <murata_shinya@ShinyaMurata.local>
2018-05-20 01:12:16 +0900

    Google API Keyを設定

--- SpotSample/SpotSample/AppDelegate.swift
+++ SpotSample/SpotSample/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyAe5-HYQuc8M-0GIdv6-DGCViRZmI-mddM")
+        GMSPlacesClient.provideAPIKey("AIzaSyBZIIA_1ItmzFCr9XNIBD45tB4jkLVoFo4")
         return true
     }
 
